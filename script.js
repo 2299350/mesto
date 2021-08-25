cfce99a66b0ea44618f1aa18341fc64a08f87f16
Alexey Litsman <alexlitsman@yandex.ru>
2021-08-25 14:28:46 +0300

    sprint 4 v.0.1

---
+++ script.js
@@ -0,0 +1,40 @@
+let pageBody = document.querySelector('.page-body');
+
+let editPopup = pageBody.querySelector('.edit-popup');
+let editPopupForm = pageBody.querySelector('.edit-popup__form');
+let shaddow = pageBody.querySelector('.page-shadow');
+
+let nameInput = editPopupForm.querySelector('#name-id');
+let jobInput = editPopupForm.querySelector('#description-id');
+
+let nameField = pageBody.querySelector('.profile__name');
+let jobField = pageBody.querySelector('.profile__description');
+
+let openPopup = pageBody.querySelector('.profile__edit-button');
+let closePopup = pageBody.querySelector('.edit-popup__close');
+
+console.log(pageBody);
+
+function popupOpenCloseHandler() {
+  shaddow.classList.toggle('states__action_hidden');
+  editPopup.classList.toggle('states__action_hidden');
+  pageBody.classList.toggle('states__action_overflow-hidden');
+
+  nameInput.value = nameField.textContent;
+  jobInput.value = jobField.textContent;
+}
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+
+    nameField.textContent = nameInput.value;
+    jobField.textContent = jobInput.value;
+
+    popupOpenCloseHandler()
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+editPopupForm.addEventListener('submit', formSubmitHandler);
+openPopup.addEventListener('click', popupOpenCloseHandler);
+closePopup.addEventListener('click', popupOpenCloseHandler);
