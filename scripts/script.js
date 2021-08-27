d09f63d3237c99d94b241d8b2d9288ec942bdefc
Alexey Litsman <alexlitsman@yandex.ru>
2021-08-27 23:18:52 +0300

    sprint 4 v.1.0 (fixes after review)

---
+++ scripts/script.js
@@ -0,0 +1,40 @@
+let pageBody = document.querySelector('.page-body');
+
+let popup = pageBody.querySelector('.popup');
+let popupForm = pageBody.querySelector('.popup__form');
+
+
+let nameInput = popupForm.querySelector('#name-id');
+let jobInput = popupForm.querySelector('#description-id');
+
+let nameField = pageBody.querySelector('.profile__name');
+let jobField = pageBody.querySelector('.profile__description');
+
+let openPopup = pageBody.querySelector('.profile__edit-button');
+let closePopup = pageBody.querySelector('.popup__close-button');
+
+function popupOpenHandler() {
+  popup.classList.toggle('popup_hidden');
+
+  nameInput.value = nameField.textContent;
+  jobInput.value = jobField.textContent;
+}
+
+function popupCloseHandler() {
+  popup.classList.toggle('popup_hidden');
+}
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+
+    nameField.textContent = nameInput.value;
+    jobField.textContent = jobInput.value;
+
+    popupCloseHandler()
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+popupForm.addEventListener('submit', formSubmitHandler);
+openPopup.addEventListener('click', popupOpenHandler);
+closePopup.addEventListener('click', popupCloseHandler);
